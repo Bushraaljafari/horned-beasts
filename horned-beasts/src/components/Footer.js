03a1280ddd9c475729808b07c10f320f8ded20c7
bushraaljafari <bushra.aljafari@gmail.com>
2021-07-25 23:33:46 +0300

    push lab-class01

---
+++ horned-beasts/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class Footer extends React.Component{
+    render(){
+        return(
+            <footer>
+                <h4> author:bushra aljafari </h4>
+            </footer>
+        )
+    }
+}
+export default Footer; 
