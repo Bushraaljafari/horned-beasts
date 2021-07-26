beb6cece2189ed03e3f73c5b17aa898b20f4a9e7
bushraaljafari <bushra.aljafari@gmail.com>
2021-07-27 02:40:22 +0300

    lab02

--- horned-beasts/src/components/HornedBeast.js
+++ horned-beasts/src/components/HornedBeast.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
 class HornedBeast extends React.Component{
+    constractor(props){
+        super(props)
+        this.stats={counter:0}
+    }
+
+    favoritImgeNum=()=>{this.setState({counter:this.state.counter+1})}
+    
     render(){
         console.log(this.props);
         return(
 
             <div>
-                <h2> {this.props.title}📓 </h2>
+               {
+               /*<h2> {this.props.title}📓 </h2>
                 <p>{this.props.description}</p>
                 <img src={this.props.img} alt={this.props.title} title={this.props.title}/>
+                <p>favorited❤️:{this.state.counter}</p>*/} 
+            
+ <Card style={{ width: '18rem' }}>
+                    
+                    <Card.Body>
+                     <Card.Img onClick={this.favoritImgeNum}  src={this.props.img}/>
+                        <Card.Title>{this.props.title}</Card.Title>
+                        <Card.Text>
+                          description📓: {this.props.description}
+                        </Card.Text>
+                        <Card.Text>
+                         favorited❤️:{this.state.counter}
+                         </Card.Text>
+                    </Card.Body>
+                </Card>
             </div>
         )
     }
